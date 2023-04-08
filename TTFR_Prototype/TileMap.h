a5725164607d809cdc4ea7b7db7618354ada8050
comradeofglory <shkilevich.mitya@yandex.ru>
2023-04-08 15:26:59 +0300

    Обновление ДД

--- TTFR_Prototype/TileMap.h
+++ TTFR_Prototype/TileMap.h
@@ -12,17 +12,17 @@ private:
 		Vector2i t_position;
 		
 		int collision;
-		//TODO:
+		//TODO:	
 		//animation
-		
+		Sprite sprite;
 		int tile_type;
 	};
 
 	struct tile_type {
 		int type;
-		Sprite sprite;
 		Texture texture;
 	};
+
 public:
 	Vector2i size;
 	int tile_type_number;
@@ -31,9 +31,7 @@ public:
 	tile_type* type = new tile_type[tile_type_number];
 
 
-
-
 	void init(); 
-	Vector2i collide(Vector2i ch_position, Vector2i ch_size, Vector2i ch_velocity, short int ch_collision); //Принимает позицию и скорость юнита, возвращает скорость юнита с учетом коллизии
+	//Vector2i collide(Vector2i ch_position, Vector2i ch_size, Vector2i ch_velocity, short int ch_collision); //Принимает позицию и скорость юнита, возвращает скорость юнита с учетом коллизии
 };
 
