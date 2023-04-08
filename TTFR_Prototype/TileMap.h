ebecd3c87cab3daec814a93144038ff0c69dfad9
comradeofglory <shkilevich.mitya@yandex.ru>
2023-04-08 13:40:29 +0300

    Реструктурировали класс Tilemap. Класс Тайл не используется, массив тайлов хранится как структура

--- TTFR_Prototype/TileMap.h
+++ TTFR_Prototype/TileMap.h
@@ -1,16 +1,39 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "Tile.h"
-
 using namespace sf;
 
 class TileMap
 {
 private:
+	struct tiles
+	{
+		Vector2i size;
+		Vector2i position;
+		Vector2i t_position;
+		
+		int collision;
+		//TODO:
+		//animation
+		
+		int tile_type;
+	};
+
+	struct tile_type {
+		int type;
+		Sprite sprite;
+		Texture texture;
+	};
 public:
 	Vector2i size;
-	Tile* tile = new Tile[size.x*size.y];
+	int tile_type_number;
+
+	tiles* tile = new tiles[size.x*size.y];
+	tile_type* type = new tile_type[tile_type_number];
+
+
+
 
 	void init(); 
+	Vector2i collide(Vector2i ch_position, Vector2i ch_size, Vector2i ch_velocity, short int ch_collision); //Принимает позицию и скорость юнита, возвращает скорость юнита с учетом коллизии
 };
 
