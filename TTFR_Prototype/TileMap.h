e038ad46bb8796c1106586a6dfeb243d86b79151
TixTheFox <kamorka18@gmail.com>
2023-04-08 19:15:45 +0700

    TileMap class

--- TTFR_Prototype/TileMap.h
+++ TTFR_Prototype/TileMap.h
@@ -1,16 +1,41 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "Tile.h"
-
 using namespace sf;
 
 class TileMap
 {
 private:
+	
+
 public:
-	Vector2i size;
-	Tile* tile = new Tile[size.x*size.y];
+	struct tiles
+	{
+		Vector2i size;
+		Vector2i position;
+		Vector2i t_position;
+		Sprite sprite;
+		
+		//TODO:
+		//animation
 
-	void init(); 
+		int tile_type;
+	};
+
+	struct tile_type {
+		int collision;
+		Texture* texture;
+	};
+
+	Vector2i size{0 ,0};
+	int tile_type_number = 0;
+
+	tiles* tile = NULL;
+	tile_type* type = NULL;
+
+
+
+	TileMap();
+
+	void init(std::string LevelFile);
+	Vector2i collide(Vector2i ch_position, Vector2i ch_size, Vector2i ch_velocity, short int ch_collision);
 };
-
