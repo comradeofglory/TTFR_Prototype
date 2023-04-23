8960c4ffbea855a7f404914994ae45c22451f6f2
comradeofglory <shkilevich.mitya@yandex.ru>
2023-04-23 14:15:23 +0300

    Добавленны классы анатомии, контроллера и инвенторя персонажа без функционала

--- TTFR_Prototype/Character.cpp
+++ TTFR_Prototype/Character.cpp
@@ -1,7 +1,7 @@
 #include "Character.h"
 
 void Character::init(Vector2f position, Vector2f size) {
-	body.init(position, size, 1, 0.18, 2);
+	body.init(position, size, 1.2, 0.105, 2);
 	body.set_velocity({ 0.f,0.f });
 	body.set_acceleration({ 0.f,0.f });
 	shape.setFillColor(Color::White);
